53070f0e7a42e5e150927869ae58312687bfc8fe
Tom Smith <Tom_Esq@Yahoo.co.uk>
2017-01-09 20:42:22 +0000

    States and Transitions

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,16 +2,26 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
 
 @Component({
   selector: 'my-app',
-  template: `<button [@myTrigger]='state'>My Button</button>`,
+  template: `<button [@myTrigger]='state' (click)='toggleState()'>My Button</button>`,
   styles: [],
   animations: [
     trigger('myTrigger', [
-      //state()
-      //transition()
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.4)'
+      })),
+      transition('small <=> large', animate('500ms'))
     ])
   ]
 })
 export class AppComponent  { 
   name = 'Angular'; 
-  state: string = 'small';
+  state: string = 'large';
+
+  toggleState() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  }
 }
+
